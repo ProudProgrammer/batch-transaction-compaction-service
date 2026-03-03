2b3f8a16a33d6f0e88174e4756687ec07bf8315e
Gábor Balázs <gbazsi82@gmail.com>
2026-03-03 16:03:36 +0100

    Adding Maven Enforcer and Cache Test

--- src/test/java/org/gaborbalazs/compaction/CompactionControllerTest.java
+++ src/test/java/org/gaborbalazs/compaction/CompactionControllerTest.java
@@ -1,14 +1,12 @@
 package org.gaborbalazs.compaction;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -19,7 +17,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ExtendWith(SpringExtension.class)
 class CompactionControllerTest {
 
     private static final String API_COMPACTION_URL = "/api/compaction";
---
+++ src/test/java/org/gaborbalazs/compaction/CompactionServiceTest.java
@@ -0,0 +1,32 @@
+package org.gaborbalazs.compaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@EnableCaching
+@SpringBootTest
+class CompactionServiceTest {
+
+    @MockitoBean
+    private Compactor compactor;
+    @Autowired
+    private CompactionService underTest;
+
+    @Test
+    void compactorCompressShouldBeCalledOnceWhenCacheIsEnabled() {
+        TransactionBatch transactionBatch = new TransactionBatch(Collections.singletonList(1));
+
+        underTest.serve(transactionBatch);
+        underTest.serve(transactionBatch);
+
+        verify(compactor, times(1)).compress(transactionBatch.transactionIds());
+    }
+}
